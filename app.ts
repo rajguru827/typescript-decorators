93503a1ecc7e7836fc36adafc122299b0f3fe269
Gururaj <17214716+rajguru827@users.noreply.github.com>
2020-04-06 17:35:35 +0530

    First commit

---
+++ app.ts
@@ -0,0 +1,49 @@
+
+function logClass(message: string): ClassDecorator {
+    console.log(`${message} evaluated`);
+    return function(constructor: Function): void {
+        console.log(`${message} called`);
+    }
+}
+
+function logProperty(message: string): PropertyDecorator {
+    console.log(`${message} evaluated`);
+    return function(target: Object, propertyKey: string): void {
+        console.log(`${message} called`);
+    }
+}
+
+function logMethod(message: string): MethodDecorator {
+    console.log(`${message} evaluated`);
+    return function(target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log(`${message} called`);
+    }
+}
+
+function logParameter(message: string): ParameterDecorator {
+    console.log(`${message} evaluated`);
+    return function(target: Object, propertyKey: string, parameterIndex: number) {
+        console.log(`${message} called`);
+    }
+}
+
+@logClass("Class Decorator")
+class Person {
+
+    private _directReports: Person[];
+
+    @logProperty("Property Decorator")
+    public emailAddress: string;
+
+    constructor(public firstName: string, public lastName: string) {
+        this._directReports = [];
+    }
+
+    @logMethod("Method Decorator")
+    public addDirectReport(@logParameter("Parameter Decorator") person: Person) {
+        this._directReports.push(person);
+    }
+
+}
+
+const person = new Person("Guru", "Raj");
